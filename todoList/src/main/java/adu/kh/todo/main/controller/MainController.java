352cb04a53b982fb9c6862244415f9f97c3c2b99
eee595398 <eee595398@gmail.com>
2023-12-06 20:09:33 +0900

    1206

---
+++ todoList/src/main/java/adu/kh/todo/main/controller/MainController.java
@@ -0,0 +1,35 @@
+package adu.kh.todo.main.controller;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// @Controller +@ResponseBody
+// json 형태로 객체 데이터로 반환
+public class MainController {
+
+	@GetMapping("/getPortNumber")
+	public List<String> getPortNumber(){
+		return Arrays.asList("서버포트는 8080","리액트포트는 3000");
+	}
+	
+	@PostMapping("/getUserInfo")
+	public String getUserInfo(@RequestBody HashMap<String, Object> map) {
+		
+		System.out.println(map.get("name"));
+		System.out.println(map.get("age"));
+		
+		String message ="데이터가 없습니다";
+		
+		if(map.get("name").equals("홍길동") && map.get("age").equals(20)) {
+			message ="홍길동님 20세 입니다.";
+		}
+		return message;
+	}
+}
